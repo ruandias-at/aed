4c3cdd39353d2071ea757dbbd86ea0f37f9cd635
Ruan Dias <ruandiasat@gmail.com>
2025-05-09 16:20:02 -0300

    commit

--- L02/Q1.cpp
+++ L02/Q1.cpp
@@ -4,9 +4,11 @@ Matrícula: 242014471
 Questão: L02Q01
 */
 
+//Importação das bibliotecas necessárias
 #include <iostream>
 #include <string>
 #include <vector>
+#include <locale>
 
 using namespace std; //Facilita o uso de comandos sem precisar colocar o namespace antes de cada um deles
 
@@ -129,66 +131,135 @@ class FormaGeometrica {
     protected:
         int base;
         int altura;
-        int lado;
+
     public:
+
+        FormaGeometrica(int base, int altura) {
+            this-> base = base;
+            this->altura = altura;
+        }
+
         int calcularArea() {
-            base * altura;
+            return base * altura;
         }
 };
 
 //Definindo a classe Retangulo, que possui os atributos públicos base e altura
-class Retangulo : FormaGeometrica {
-    public:
-        int base;
-        int altura;
+class Retangulo : public FormaGeometrica {
 
-    //Constructor da classe Retangulo, que inicializa os atributos com os valores passados como parâmetro
-    Retangulo(int base, int altura) {
-        this->base = base;
-        this->altura = altura;
+    public:
+        //Constructor da classe Retangulo, que inicializa os atributos com os valores passados como parâmetro
+        Retangulo(int base, int altura) : FormaGeometrica(base, altura){
     }
 
-    //Método que retorna a área do retângulo, multiplicando a base pela altura
+}; //End Class
+
+
+class Quadrado : public FormaGeometrica {
+
+    public:
+    Quadrado(int lado) : FormaGeometrica(lado, lado){
+    }
+    
     int calcularArea() {
-        return base * altura;
+        return base * base;
     }
-}; //End Class
-
-//Definindo a classe Círculo, que também possui os atributos públicos base e altura
-class Circulo : FormaGeometrica {
-    public:
-        int base;
-        int altura;
-
-    //Constructor da classe Circulo, que inicializa os atributos com o valor do diâmetro passado como parâmetro
-    Circulo(int diametro) {
-        this->base = this->altura = diametro;
-    }
-
-    //Método que retorna a área do círculo, utilizando a fórmula pi * (r^2), onde r é o raio (metade do diâmetro)
-    double calcularArea() {
-        double raio = base / 2; 
-        return 3.14 * raio * raio;
-    }
-}; //End Class
-
-class Quadrado : FormaGeometrica {
-    public:
-        int lado;
-
-    Quadrado(int lado) {
-        this->lado = lado;
-    }
-
-    double calcularArea() {}
 };
 
-int main() {
+//Definindo a classe Círculo, que também possui os atributos públicos base e altura
+class Circulo : public FormaGeometrica {
+    protected:
+        int raio;
 
+    public:
+        //Constructor da classe Circulo, que inicializa os atributos com o valor do diâmetro passado como parâmetro
+        Circulo(int diametro) : FormaGeometrica (base, altura) {
+            this->raio = diametro / 2;
+        }
+
+        //Método que retorna a área do círculo, utilizando a fórmula pi * (r^2), onde r é o raio (metade do diâmetro)
+        double calcularArea() {
+            return 3.14 * raio * raio;
+        }
+}; //End Class
+
+class Funcionario {
+
+    protected:
+        string nome;
+        double salario;
+
+    public:
+        Funcionario(string nome, double salario) {
+            this->nome = nome;
+            this->salario = salario;
+        }
+
+        void setNome(string nome) {
+            this->nome = nome;
+        }
+
+        string getNome() {
+            return nome;
+        }
+
+        void setSalario(double salario) {
+            this->salario = salario;
+        }
+
+        double getSalario() {
+            return salario;
+        }
+}; //End Class
+
+class Gerente : public Funcionario {
+    
+    protected:
+        double bonus;
+
+    public:
+        Gerente(string nome, double salario, double bonus) : Funcionario(nome, salario) {
+            this->bonus = bonus;
+        }
+
+        void setBonus(double bonus) {
+            this->bonus = bonus;
+        }
+
+        double getBonus() {
+            return bonus;
+        }
+}; //End Class
+
+
+int main() {
+    
+    setlocale(LC_ALL, "Portuguese"); //Definição de local para aceitar caracteres especiais
+
+    //Instanciando um objeto da classe Pessoa e exibindo suas informações
     Aluno a1 = Aluno("Ruan", "242014471", 18, "Aluno de Redes");
     a1.adicionarNotas(9.0, 8.0);
     a1.imprime();
+
+    cout << "----------------------------------------" << endl;
+
+    //Instanciando um objeto de cada classe herdade pela classe FormaGeometrica
+    Quadrado q1 = Quadrado(5);
+    cout << "Area do quadrado: " << q1.calcularArea() << endl;
+    Retangulo r1 = Retangulo(5, 10);
+    cout << "Area do retangulo: " << r1.calcularArea() << endl;
+    Circulo c1 = Circulo(10);
+    cout << "Area do circulo: " << c1.calcularArea() << endl;
     
+    cout << "----------------------------------------" << endl;
+
+    //Instanciando um objeto da classe Funcionario e um da classe Gerente
+    Funcionario f1 = Funcionario("Ruan", 1000);
+    Gerente g1 = Gerente("Marcelo", 2000, 500);
+
+    //Exibindo as informações do funcionário e do gerente
+    cout << "Funcionario: " << f1.getNome() << ", Salario: " << f1.getSalario() << endl;
+    cout << "Gerente: " << g1.getNome() << ", Salario: " << g1.getSalario() << ", Bonus: " << g1.getBonus() << endl;
 
     return 0;
 }
